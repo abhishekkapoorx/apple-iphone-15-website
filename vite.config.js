037777bec64ac5e15dab01ae38320b879811e9b5
Abhishek Kapoor <kapoorkk1980@gmail.com>
2025-01-10 12:45:56 +0530

    Update homepage in package.json and adjust base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/apple-iphone-15-website/"
+  base: "/apple-iphone-15-website"
 })
